7df2452b3e10bd62ea14fa32cc161abe12fe7af4
MadhurShrivastav <madhurshrivastav1950@gmail.com>
2026-09-06 23:05:10 +0530

    Added url to allowed hosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ["glistening-alignment-production-cd66.up.railway.app"],
+  },
 });
